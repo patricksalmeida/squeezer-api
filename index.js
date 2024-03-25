a6ff7941df005c23fb5191c0b0410be50d02bae9
Patrick Almeida <patricksalmeida@gmail.com>
2024-03-24 22:12:18 -0300

    feat: replace static text response

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Squeezer.')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Squeezer listening on port ${port}`)
 })
